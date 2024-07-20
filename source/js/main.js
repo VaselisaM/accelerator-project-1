46599e128541e16e8094c51a50b20969500e2417
VaselisaM <ralkina.vaselisa@gmail.com>
2024-07-20 08:25:20 +0500

    About

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,29 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+const video = document.querySelector('.video');
+const button = document.querySelector('.video__button');
+const picture = document.querySelector('.about__preview');
+
+const idVideo = video.dataset.video;
+
+const createIframe = () => {
+  const iframe = document.createElement('iframe');
+  video.appendChild(iframe);
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('src', `https://www.youtube.com/embed/${idVideo}`);
+  iframe.setAttribute('allow', 'autoplay; encrypted-media');
+  picture.remove();
+  button.remove();
+
+  button.removeEventListener('click', onButtonClick);
+};
+
+function onButtonClick () {
+  createIframe();
+}
+
+button.addEventListener('click', onButtonClick);
+
